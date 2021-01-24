67a358d72d0c00550342eae21cd993378081aecd
lrwebdev93 <75271909+lrwebdev93@users.noreply.github.com>
2021-01-24 12:57:41 +0000

    Responsive - Complete

--- resources/js/script.js
+++ resources/js/script.js
@@ -24,28 +24,41 @@ $(document).ready(function () {
     });
 
     /* Animation on Scroll */
-    $('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').waypoint(function (direction) {
         $('.js--wp-1').addClass('animate__animated animate__fadeInUp')
     }, {
-        offset:'50%'
+        offset: '50%'
     })
 
-    $('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').waypoint(function (direction) {
         $('.js--wp-2').addClass('animate__animated animate__fadeInUp')
     }, {
-        offset:'50%'
+        offset: '50%'
     })
 
-    $('.js--wp-3').waypoint(function(direction) {
+    $('.js--wp-3').waypoint(function (direction) {
         $('.js--wp-3').addClass('animate__animated animate__fadeInUp')
     }, {
-        offset:'50%'
+        offset: '50%'
     })
 
-    $('.js--wp-4').waypoint(function(direction) {
+    $('.js--wp-4').waypoint(function (direction) {
         $('.js--wp-4').addClass('animate__animated animate__pulse')
     }, {
-        offset:'50%'
+        offset: '50%'
     })
 
+    /* Navigation Toggle */
+    $(".js--nav-icon").click(() => {
+        const nav = $(".js--main-nav");
+        nav.slideToggle(200); /* def uses display: !value + css slide drawer */
+        const icon = $(".js--nav-icon ion-icon");
+        console.log(icon[0].name);
+
+        if (icon.attr("name") == "menu-outline") {
+            icon.attr("name", "close-outline");
+        } else {
+            icon.attr("name", "menu-outline")
+        }
+    });
 });
