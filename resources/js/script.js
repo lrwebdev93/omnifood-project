c856e3ed0469c781eb016494dea02295a1eb663e
lrwebdev93 <75271909+lrwebdev93@users.noreply.github.com>
2021-01-24 01:06:53 +0000

    Animations on Scroll Complete

--- resources/js/script.js
+++ resources/js/script.js
@@ -13,12 +13,39 @@ $(document).ready(function () {
 
     /* Scroll to Element*/
     $('.js--scroll-to-plans').click(function () {
-        $('html,body').animate({ scrollTop: $('.js--section-plans').offset().top},1000)
+        $('html,body').animate({
+            scrollTop: $('.js--section-plans').offset().top
+        }, 1000)
     })
-
     $('.js--scroll-to-start').click(function () {
-        $('html,body').animate({ scrollTop: $('.js--section-features').offset().top},1000)
+        $('html,body').animate({
+            scrollTop: $('.js--section-features').offset().top
+        }, 1000)
+    });
+
+    /* Animation on Scroll */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animate__animated animate__fadeInUp')
+    }, {
+        offset:'50%'
     })
 
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animate__animated animate__fadeInUp')
+    }, {
+        offset:'50%'
+    })
+
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animate__animated animate__fadeInUp')
+    }, {
+        offset:'50%'
+    })
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animate__animated animate__pulse')
+    }, {
+        offset:'50%'
+    })
 
 });
